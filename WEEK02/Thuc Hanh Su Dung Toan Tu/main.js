15f73d9166c25e0bb9fe960570ed2b6e62ef64dd
nguyenduc11 <nguyenduc11@gmail.com>
2022-09-07 14:49:14 +0700

    2022-0907 Commit2
    Thuc Hanh Su Dung Toan Tu

---
+++ WEEK02/Thuc Hanh Su Dung Toan Tu/main.js
@@ -0,0 +1,4 @@
+let width = parseInt(prompt("Please input the Width"));
+let height = parseInt(prompt("Please input the Height"));
+let area = width * height;
+document.write("The area is: " + area);
