0a250e745dc9f8376b5d03d2e531af8706bb8d90
nguyenduc11 <nguyenduc11@gmail.com>
2022-09-07 13:55:28 +0700

    2022-0907 Commit1
    2022-0907 Commit1

--- WEEK02/Thuc Hanh Khai Bao Va Su Dung Bien/main.js
+++ WEEK02/Thuc Hanh Khai Bao Va Su Dung Bien/main.js
@@ -1,11 +1,34 @@
+//Bai 1//
+document.write("<div style='font-size:30px'>Bài 01</div>");
 let i = 10;
 let f = 20.5;
-let b = true;
+let bb = true;
 let s = "Hà Nội";
 
 document.write("i = " + i + "<br>");
 document.write("f = " + f + "<br>");
-document.write("b = " + b + "<br>");
-document.write("s = " + s + "<br>");
+document.write("b = " + bb + "<br>");
+document.write("s = " + s + "<br>" + "<br>");
 
+//Bai 2//
+document.write("<div style='font-size:30px'>Bài 02</div>");
+let RecWidth = 100;
+let RecHeight = 20;
+let RecArea = RecWidth * RecHeight;
+document.write("Rectangular Width = " + RecWidth + "<br>");
+document.write("Rectangular Height = " + RecHeight + "<br>");
+document.write("Rectangular Area = " + RecArea + "<br>" + "<br>");
 
+//Bai 3//
+document.write("<div style='font-size:30px'>Bài 03</div>");
+let a = prompt("Enter 1st Number a = ");
+document.write("Your 1st Number is " + a +"<br>")
+let b = prompt("Enter 2nd Number b =");
+document.write("Your 2nd Number is " + b +"<br>")
+if (a%b == 0){
+    alert("a là bội số của b");
+    document.write(a + " là bội số của " + b)
+} else {
+    alert("a không là bội số của b");
+    document.write(a + " không là bội số của " + b)
+}
