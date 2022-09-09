a1e590e0a1583c8f1a432a2bcbc59f4803b164ea
nguyenduc11 <nguyenduc11@gmail.com>
2022-09-09 16:09:40 +0700

    2022-0909 Commit1
    Thuc hanh su kien chuot

---
+++ WEEK02/Bai Tap Bien Kieu Du Lieu Va Toan Tu/main.js
@@ -0,0 +1,15 @@
+//Bai 1//
+document.write("<div style='font-size:30px'>Bài 01</div>");
+/*let DiemVatLy = parseInt(prompt("Nhập điểm Vật Lý của bạn"));
+let DiemHoaHoc = parseInt(prompt("Nhập điểm Hóa Học của bạn"));
+let DiemSinhHoc = parseInt(prompt("Nhập điểm Sinh Học của bạn"));
+let TongDiem = DiemVatLy + DiemHoaHoc + DiemSinhHoc;
+let DiemTrungBinh = Number((TongDiem / 3).toFixed(1));
+document.write("Điểm Trung Bình: " + DiemTrungBinh + "<br>");
+document.write("Tổng Điểm: " + TongDiem + "<br>" + "<br>");*/
+
+//Bai 2//
+document.write("<div style='font-size:30px'>Bài 02</div>");
+let Celsius = parseInt(prompt("Nhập giá trị nhiệt độ C"));
+let Fahrenheit = (Celsius*9/5) + 32;
+document.write()
